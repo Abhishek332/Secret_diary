381c0cd70b31e1d3961401727b2a27afda625b7a
abhishek <abhishekp2k19@gmail.com>
2021-08-20 09:36:14 +0530

    Fully Responsive Secret Diary Build, Free to use it

--- signup.php
+++ signup.php
@@ -1,43 +1,40 @@
-<?php require "config.php";
+<?php require "assets/config.php";
 
 if(isset($_POST['signup'])!=null){
     $email = isset($_POST['s-email'])?addslashes(strip_tags(trim($_POST['s-email']))):null;
     $password = isset($_POST['s-password'])?addslashes(strip_tags(trim($_POST['s-password']))):null;
 
-    if ($email==null || $password==null) {
+    if($email==null || $password==null) {
         session_destroy();
-        header("Location : ./index.php");
+        $msg = "Please Fill Input Fields";
+        $backto = "./?msg=$msg";
     }else {
-
         // Check whether this email exists
         $query = "SELECT * FROM users WHERE email='$email'";
         $result = mysqli_query($con, $query);
         $numExistRows = mysqli_num_rows($result);
-        if($numExistRows > 0){
-            // echo "<script>alert('Email Already Used Please Use a Different One');</script>";
-            // header("Location : ./index.php");
-            ?>
-            <script type="text/javascript">
-                alert('Email Already Used Please Use a Different One');
-                window.location="../index.php";
-            </script>
-           <?php 
+        if($numExistRows){
+            $msg = "Email Already Used Please Use a Different One";
+            $backto = "./?msg=$msg";
         }
         else{
             $hash = password_hash($password, PASSWORD_DEFAULT);
             $sql = "INSERT INTO users ( email, password) VALUES ('$email', '$hash')";
             $result = mysqli_query($con, $sql);
             if ($result){
-                ?>
-            <script type="text/javascript">
-                alert('Register Successful, you can now login');
-                window.location="../index.php";
-            </script>
-           <?php 
+                $_SESSION['UID'] = mysqli_insert_id($con);
+                $msg = "Registration Successful";
+                $backto = "./?msg=$msg&mydiary"; 
             }
         }
     }
 
-}
-            
+    if($backto != NULL){
+?>
+    <script>
+        window.location="<?php echo $backto; ?>"
+    </script>
+<?php
+    }
 
+}
